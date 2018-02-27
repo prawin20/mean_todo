9a9e3327b106e27989a29467152f35624ceb83df
Praveen <prawin20@gmail.com>
2018-02-27 10:27:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override")
 var mongoose = require("mongoose");
+var compression = require('compression')
 require("dotenv").config();
 
 
@@ -11,7 +12,7 @@ mongoose.connect(db.url);
 var port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
-app.use(express.compress());
+app.use(compression());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(methodOverride('X-HTTP-Method-Override')); 
