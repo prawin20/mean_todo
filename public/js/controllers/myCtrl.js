d66b82af235ee0b31349191cf8f240a8338f8222
Praveen <prawin20@gmail.com>
2018-03-09 19:16:43 +0530

    Update myCtrl.js

--- public/js/controllers/myCtrl.js
+++ public/js/controllers/myCtrl.js
@@ -3,17 +3,17 @@
 var myApp=angular.module("myApp",[]);
 myApp.controller("myCtrl",function($scope,$http){
     $http.get("/api/name").then(function(response){
-        $scope.n=response.data;
+        $scope.names=response.data;
     });
     $scope.create=function(){
         $http.post("/api/name",$scope.name).then(function(response){
-        $scope.n=response.data;
+        $scope.names=response.data;
     })
     $scope.name="";
     };
     $scope.remove=function(id){
         $http.delete("/api/name/"+id).then(function(response){
-        $scope.n=response.data;
+        $scope.names=response.data;
     })
     };
 })
